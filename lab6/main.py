ad3001f2de569e0221d40eb73c888ac429dcd755
Mykhailo Briazkalo <mihialstudykpi@gmail.com>
2023-03-25 17:19:31 +0100

    lab6

--- lab6/main.py
+++ lab6/main.py
@@ -1,53 +1,86 @@
-def KSA(key):
-    S = list(range(256))
+from typing import List
+import time
+
+from docx import Document
+import pandas as pd
+
+
+def ksa(key: str) -> List[int]:
+    """Key Scheduling Algorithm"""
+    s = list(range(256))
 
     j = 0
     for i in range(256):
-        j = (j + S[i] + ord(key[i % len(key)])) % 256
-        S[i], S[j] = S[j], S[i]
-    return S
+        j = (j + s[i] + ord(key[i % len(key)])) % 256
+        s[i], s[j] = s[j], s[i]
+    return s
 
 
-def PRGA(S, data):
-    out = []
+def prga(s: List[int], data_len: int) -> List[int]:
+    """Pseudo-Random Generation Algorithm"""
+    ks = []
     j = 0
     i = 0
-    for char in data:
+    for _ in range(data_len):
         i = (i + 1) % 256
-        j = (j + S[i]) % 256
-        S[i], S[j] = S[j], S[i]
-        out.append(
-            ord(char) ^ S[(S[i] + S[j]) % 256]
-        )
+        j = (j + s[i]) % 256
+        s[i], s[j] = s[j], s[i]
+        ks.append(s[(s[i] + s[j]) % 256])
+    return ks
+
+
+def encrypt(msg: str, key: str) -> List[int]:
+    s = ksa(key)
+    ks = prga(s, len(msg))
+    out = []
+    for char, k in zip(msg, ks):
+        out.append(ord(char) ^ k)
     return out
 
-def d_PRGA(S, data):
+
+def decrypt(msg: List[int], key: str) -> List[int]:
+    S = ksa(key)
+    ks = prga(S, len(msg))
     out = []
-    j = 0
-    i = 0
-    for char in data:
-        i = (i + 1) % 256
-        j = (j + S[i]) % 256
-        S[i], S[j] = S[j], S[i]
-        out.append(
-            char ^ S[(S[i] + S[j]) % 256]
-        )
+    for char, k in zip(msg, ks):
+        out.append(char ^ k)
     return out
 
 
 def main():
-    msg = 'hello this is test message'
-    key = 'mykey'
+    key = "7179875849"
 
-    b_msg = msg.encode()
-    b_key = key.encode()
+    document = Document("medium.docx")
+    with open("medium.txt", mode="w") as f:
+        f.write(document.paragraphs[0].text)
 
-    S = KSA(key)
-    print([hex(ord(i)) for i in msg])
-    encrypted_msg = PRGA(S, msg)
-    S = KSA(key)
-    print([hex(i) for i in encrypted_msg])
-    print([hex(i) for i in d_PRGA(S, encrypted_msg)])
+    stats = {}
+    for file_path in ("short.txt", "medium.txt", "long.txt"):
+        stats[file_path] = {}
+        stats[file_path][key] = {}
 
-if __name__ == '__main__':
+        with open(file_path, mode="r") as f:
+            msg = f.read()
+
+        print(f"Original message:\n{msg}")
+        encryption_start = time.perf_counter()
+        encrypted_msg = encrypt(msg, key)
+        stats[file_path][key]["Encryption time"] = (
+            time.perf_counter() - encryption_start
+        )
+        print(f"Encrypted message:\n{''.join([chr(i) for i in encrypted_msg])}")
+
+        decryption_start = time.perf_counter()
+        decrypted_msg = decrypt(encrypted_msg, key)
+        stats[file_path][key]["Decryption time"] = (
+            time.perf_counter() - decryption_start
+        )
+        print(f"Decrypted message:\n{''.join([chr(i) for i in decrypted_msg])}")
+    df = pd.concat(
+        {k: pd.DataFrame.from_dict(v, "index") for k, v in stats.items()}, axis=0
+    )
+    df.round(6).to_csv("df.csv")
+
+
+if __name__ == "__main__":
     main()
